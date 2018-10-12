8531c0eaa8e464b0e7d20e20e425d4a818690058
santiago <santiagoconde03@hotmail.com>
2018-10-12 17:00:26 -0500

    Update data and index

--- js/sactterplot.js
+++ js/sactterplot.js
@@ -1,103 +1,52 @@
-var url2010 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2010.csv",
-  url2011 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2011.csv",
-  url2012 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2012.csv",
-  url2013 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2013.csv",
-  url2014 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2014.csv",
-  url2015 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2015.csv",
-  url2016 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2016.csv",
-  url2017 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2017.csv",
-  url2018 = "https://raw.githubusercontent.com/santiagoconde0/EstupefacientesColombia/master/data/2018.csv";
+ // margenes
+ margin = {
+     top: 20,
+     right: 30,
+     bottom: 30,
+     left: 100
+   },
+   width = 790,
+   height = 450,
+   radius = Math.min(width, height) / 2,
+   iwidth = width - margin.left - margin.right,
+   iheight = height - margin.top - margin.bottom;
 
-// llamar fata del 2010
+ // crear svg
+ var svg = d3
+   .select("#target1")
+   .append("svg")
+   .attr("width", width + margin.left + margin.right)
+   .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-let y = 0;
+ // Escala eje Y
+ y = d3.scaleLinear()
+   .domain([d3.min(nested_data, d => d.value), d3.max(nested_data, d => d.value)]).nice()
+   .range([height - margin.bottom, margin.top]);
 
-d3.csv(url2010).then(data => {
-  // console.log("url 2010:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-  y = max;
-});
-
-
-d3.csv(url2011).then(data => {
-  // console.log("url 2011:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2012).then(data => {
-  // console.log("url 2012:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2013).then(data => {
-  // console.log("url 2013:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2014).then(data => {
-  // console.log("url 2014:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2015).then(data => {
-  // console.log("url 2015:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2016).then(data => {
-  // console.log("url 2016:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2017).then(data => {
-  // console.log("url 2017:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
-d3.csv(url2018).then(data => {
-  // console.log("url 2018:", data);
-
-  // valores extremos
-  var min = d3.min(data, d => +d.CANTIDAD),
-    max = d3.max(data, d => +d.CANTIDAD);
-  console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
-});
+ // crear eje y
+ svg.append("g")
+   .attr("transform", `translate(${margin.left},0)`)
+   .call(d3.axisLeft(y));
 
 
-var i = 0; //anio min
+ // Llamar data
+ d3.csv("/data/2010-2018.csv").then(data => {
+   console.log("total data: ", data);
+   // valores extremos
+   var min = d3.min(data, d => +d.CANTIDAD),
+     max = d3.max(data, d => +d.CANTIDAD);
+   console.log("Cantidad minima: " + min + " y Cantidad maxima: " + max);
 
-  function myLoop() {
-    setTimeout(function() {
-console.log("Y:", y);
-      i++;
-      if (i < 5) { //anio max
-        myLoop();
-      }
-    }, 500)
-  }
-  myLoop();
+
+   // Escala eje Y
+   y = d3.scaleLinear()
+     .domain([min, max]).nice()
+     .range([height - margin.bottom, margin.top]);
+
+   // crear eje y
+   svg.append("g")
+     .attr("transform", `translate(${margin.left},0)`)
+     .call(d3.axisLeft(y));
+ });
